8c7d3fb09f9684b65ada7e3ef623adc514bc1b00
Martin Lin <martin91822@yahoo.com.tw>
2026-02-13 01:04:42 +0800

    multer problems3

--- animal_adoption_app/backend/index.js
+++ animal_adoption_app/backend/index.js
@@ -8,33 +8,24 @@ import adminRoutes from './routes/admin.js';
 import recommendRoutes from "./routes/recommend.js";
 import adoptRouter from './routes/adopt.js';
 
+
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// -----------------------------
-// CORS 設定
-// -----------------------------
-app.use(cors({
-  origin: "https://animal-react-adoption.vercel.app", // 只允許你的前端網域
-  methods: ["GET", "POST", "PUT", "DELETE"],         // 允許 HTTP 方法
-  credentials: true                                  // 如果你前端要傳 cookie
-}));
-
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // 若前端使用 FormData 會用到
 
-// -----------------------------
+
 // 路由
-// -----------------------------
 app.use('/user', userRoutes);
 app.use('/animals', animalRoutes);
 app.use('/admin', adminRoutes);
 app.use("/api", recommendRoutes);
 app.use('/', adoptRouter);
 
-// -----------------------------
 // 啟動
-// -----------------------------
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
