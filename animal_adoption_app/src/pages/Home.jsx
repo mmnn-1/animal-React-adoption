ebbeb706db6fc9a6110ceea9850359c38a1d4c79
Martin Lin <martin91822@yahoo.com.tw>
2026-02-25 17:20:03 +0800

    update Home.css

--- animal_adoption_app/src/pages/Home.jsx
+++ animal_adoption_app/src/pages/Home.jsx
@@ -4,25 +4,26 @@ import AnimalList from "../components/AnimalList";
 import MapSection from "../components/MapSection";
 
 import "./Home.css";
+import dogImage from "../assets/dog.jpg";
 
 function Home() {
   return (
     <div className="home">
       <Header />  {/* 🟢 這裡加入導覽列 */}
       <NewsMarquee />
-      <section className="hero">
-        <h1 className="hero-title">給牠一個家 🐾</h1>
-        <p className="hero-subtitle">
-          目前有這些可愛的浪浪，正在等待你的擁抱
-        </p>
-      </section>
-      <section className="content">
-        <AnimalList />
-      </section>
-      <section className="map-section">
-        <h2>收養地點分佈</h2>
-        <MapSection />
-      </section>
+     <section className="hero">
+  <div className="hero-text">
+    <h1 className="hero-title">給牠一個家 🐾</h1>
+    <p className="hero-subtitle">
+      目前有這些可愛的浪浪，正在等待你的擁抱
+    </p>
+    <button className="hero-btn">立即看看浪浪</button>
+  </div>
+
+  <div className="hero-image">
+    <img src={dogImage} alt="可愛狗狗" />
+  </div>
+</section>
     </div>
   );
 }
